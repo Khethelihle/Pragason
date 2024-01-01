45dc06bd0e6b03b5644a470303d0a2e4c711a163
mlambokm <mlambokm@amazon.com>
2024-01-01 13:59:51 +0200

    first commit

---
+++ src/main/java/com/awsprojrct/pragason/AwsProjectPragasonBackendApiApplication.java
@@ -0,0 +1,15 @@
+package com.awsprojrct.pragason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectPragasonBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsProjectPragasonBackendApiApplication.class, args);
+
+        System.out.print("Code Setup Complete !");
+    }
+
+}
