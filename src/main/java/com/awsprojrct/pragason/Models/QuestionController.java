7567a0516c012f5677d0db031421a74fe7c39a04
ec2-user <mlambokt@gmail.com>
2024-04-28 09:24:52 +0000

    Resolving Dependencies

--- src/main/java/com/awsprojrct/pragason/Models/QuestionController.java
+++ src/main/java/com/awsprojrct/pragason/Models/QuestionController.java
@@ -8,9 +8,7 @@ import java.util.List;
 
 @RestController
 public class QuestionController {
-
-//    Question question = new Question(1, "Customer got the following error when trying to create Indexes — INVALID_PARTITION_TYPE_DATA_ERROR", "Attempt01","Data Catalog","Observed when the partitionKey value is not a valid value for the corresponding partitionKey data type.", "Observed when the partitionKey value is not a valid value for the corresponding partitionKey data type,Observed When the parttitionKey is encrypted,Observed when the partition key in S3 has been deleted and a table is not updated by the crawler,Observed when the console role does not have CreatePartitionIndex permissions");
-
+    
     private final QuestionRepository questionRepository;
 
     public QuestionController(QuestionRepository questionRepository) {
